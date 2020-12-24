3abe5de6126d02d6807764e17c85e7016b9ce570
Brenda-cbp <bc.barahona@uniandes.edu.co>
2020-12-24 10:19:08 -0500

    Cambios topicos

--- Robocol/testapp/src/app/rover/estado/estado.module.ts
+++ Robocol/testapp/src/app/rover/estado/estado.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EstadoPestaniaComponent } from './estado-pestania/estado-pestania.component';
 import { EstadoTopicosComponent } from './estado-topicos/estado-topicos.component';
+import { LiveComponent } from '../traccion/live/live.component';
+import { VideoComponent } from './video/video.component'
+
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,    
   ],
-  declarations: [EstadoPestaniaComponent, EstadoTopicosComponent],
-  exports:[ EstadoPestaniaComponent]
+  declarations: [EstadoPestaniaComponent, EstadoTopicosComponent, VideoComponent],
+  exports:[ EstadoPestaniaComponent,VideoComponent]]
 })
 export class EstadoModule { }
