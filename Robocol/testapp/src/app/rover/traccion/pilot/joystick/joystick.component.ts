a8511a73b623218f40c45db981b418710c6a2d75
Alvaro Plata <ad.plata@uniandes.edu.co>
2021-09-21 18:29:43 -0500

    Add files via upload

---
+++ Robocol/testapp/src/app/rover/traccion/pilot/joystick/joystick.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-joystick',
+  templateUrl: './joystick.component.html',
+  styleUrls: ['./joystick.component.css']
+})
+export class JoystickComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Robocol/testapp/src/app/rover/traccion/pilot/joystick/joystick.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JoystickComponent } from './joystick.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [JoystickComponent]
+})
+export class JoystickModule { }
