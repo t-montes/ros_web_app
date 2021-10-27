2ab274f7d3ff449aff418102733adf659f24d88d
alvarodpm <ad.plata@uniandes.edu.co>
2021-10-27 13:11:32 -0500

    sistema extraccion ready

---
+++ Robocol/testapp/src/app/rover/brazo/sistema-extraccion-b/sistema-extraccion-b.component.ts
@@ -0,0 +1,30 @@
+import { BrazoService } from '../brazo.service';
+import { WebsocketService } from '../../../websocket.service';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sistema-extraccion-b',
+  templateUrl: './sistema-extraccion-b.component.html',
+  styleUrls: ['./sistema-extraccion-b.component.css'],
+  providers: [WebsocketService, BrazoService],
+})
+export class SistemaExtraccionBComponent implements OnInit {
+
+  @Input() brazoService: BrazoService;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  send_selection(): void {
+    const command = document.getElementById("command") as HTMLSelectElement;
+    const message = {
+      id: "sistema_extraccion_b",
+      command: parseInt(command.value)
+    }
+    this.brazoService.messages.next(message);
+    console.log('new message from brazo to websocket: ', message);
+  }
+
+}
