7b397e07b87908f4da861afe035f9082c00d49e7
alvarodpm <ad.plata@uniandes.edu.co>
2021-11-01 19:32:34 -0500

    ROS_HOSTNAME not necessary

--- docker_instructions.sh
+++ docker_instructions.sh
@@ -2,13 +2,10 @@
 . /opt/ros/melodic/setup.bash
 cd ../..
 source devel/setup.bash
-export ROS_HOSTNAME=$IP_ADDRESS
 export ROS_IP=$IP_ADDRESS
 echo 1
 echo $IP_ADDRESS
 echo 2
-echo $ROS_HOSTNAME
-echo 3
 echo $ROS_IP
-echo 4
+echo 3
 roslaunch ros_web_app_2 launch_GUI.launch
