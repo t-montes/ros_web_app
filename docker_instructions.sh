cbc2356037817f20679abaf475724a8a9f9da4d6
alvarodpm <ad.plata@uniandes.edu.co>
2021-11-01 19:27:27 -0500

    Test ros export debug

--- docker_instructions.sh
+++ docker_instructions.sh
@@ -4,4 +4,11 @@ cd ../..
 source devel/setup.bash
 export ROS_HOSTNAME=$IP_ADDRESS
 export ROS_IP=$IP_ADDRESS
+echo 1
+echo $IP_ADDRESS
+echo 2
+echo $ROS_HOSTNAME
+echo 3
+echo $ROS_IP
+echo 4
 roslaunch ros_web_app_2 launch_GUI.launch
