99b1ece2d92aaadd02997188f6a2a3b9cf715777
Alvaro Plata <ad.plata@uniandes.edu.co>
2021-11-03 19:48:16 -0500

    Update docker_instructions.sh

--- docker_instructions.sh
+++ docker_instructions.sh
@@ -3,9 +3,5 @@
 cd ../..
 source devel/setup.bash
 export ROS_IP=$IP_ADDRESS
-echo 1
-echo $IP_ADDRESS
-echo 2
-echo $ROS_IP
-echo 3
+export ROS_MASTER_URI=http://${IP_ROS_MASTER}:11311/
 roslaunch ros_web_app_2 launch_GUI.launch
